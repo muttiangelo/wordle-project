d612923a8e9d2c5abc20390f11ff193cd369a53b
asmutti <amutti.dev@gmail.com>
2022-03-01 16:50:13 -0300

    att

--- wordle.py
+++ wordle.py
@@ -1,22 +1,31 @@
 from colors_lib import bcolors
 import random
 
+lista_de_palavras = []
+l = open("Lista-de-Palavras.txt", "r")
+for i in range(len(l)):
+    # if(len(l.readline(i)) == 5):
+    lista_de_palavras.append(l.readline(i))
 
-def wordle(word_input = input('Digite uma palavra: ')):
-    lista = ["navio", "tenis", "mesmo", "carro"]
-
-    word = lista[random.randint(0, 3)]
+lista = ["navio", "tenis", "mesmo", "carro"]
+word = lista_de_palavras[random.randint(0, len(lista_de_palavras))]
+def wordle():
+    word_input = input('Digite uma palavra: ')
 
     if len(word_input) == 5:
         for i in range(len(word_input)):
             if word_input[i] == word[i]:
                 print(f"{bcolors.OKGREEN}" + word_input[i] + bcolors.ENDC)
+            elif word_input[i] in word:
+                print(f"{bcolors.WARNING}" + word_input[i] + bcolors.ENDC)
             else:
                 print(f"{bcolors.RED}" + word_input[i] + bcolors.ENDC)
     else:
         print(f"{bcolors.WARNING}" + "palavra precisa conter 5 letras" + bcolors.ENDC)
+# wordle()
 
-    # if(word_input == word):
-    #     print(f"{bcolors.OKGREEN}" + word + bcolors.ENDC)
+tentativas = 0
 
-wordle()
+while(tentativas < 6):
+    wordle()
+    tentativas += 1
