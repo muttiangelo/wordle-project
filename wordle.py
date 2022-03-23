1d58faf258523240098b692e646c769988bb4e4a
muttiangelo <amutti.dev@gmail.com>
2022-03-22 23:40:53 -0300

    break quando acerta e excede tentativas adicionado

--- wordle.py
+++ wordle.py
@@ -5,31 +5,28 @@ lista = ["navio", "tenis", "mesmo", "carro"]
 word = lista[random.randint(0, len(lista) -1)]
 
 def wordle():
-    word_input = input("Digite uma palavra: ")
-    if len(word_input) == 5:
-        tentativas = 0
+    tentativas = 0
+    while True:
         palavra = ""
-        for i in range(len(word_input)):
-            if word_input[i] == word[i]:
-                palavra = palavra + f"{bcolors.OKGREEN}" + word_input[i] + bcolors.ENDC
-            elif word_input[i] in word:
-                palavra = palavra + f"{bcolors.WARNING}" + word_input[i] + bcolors.ENDC
-            else:
-                palavra = palavra + f"{bcolors.RED}" + word_input[i] + bcolors.ENDC
+        if tentativas > 6:
+            print("Numero de tentativas excedido. A palavra era", word)
+            break
+        word_input = input("Digite uma palavra: ")
+        if len(word_input) == 5:
+            for i in range(len(word_input)):
+                if word_input[i] == word[i]:
+                    palavra = palavra + f"{bcolors.GREEN}" + word_input[i] + bcolors.ENDC
+                elif word_input[i] in word:
+                    palavra = palavra + f"{bcolors.YELLOW}" + word_input[i] + bcolors.ENDC
+                else:
+                    palavra = palavra + f"{bcolors.RED}" + word_input[i] + bcolors.ENDC
+            print(palavra)
             tentativas+=1
-        print(palavra)
-        if word_input == word:
-            print("Voce acertou")
+            if word_input == word:
+                print("Você acertou")
+                break
+            
+        else:
+            print(f"{bcolors.YELLOW}" + "palavra precisa conter 5 letras" + bcolors.ENDC)
 
-    else:
-        print(f"{bcolors.WARNING}" + "palavra precisa conter 5 letras" + bcolors.ENDC)
-
-tentativas = 0
-
-
-while(True):
-    wordle()
-    tentativas +=1
-    if tentativas == 6:
-        print("Voce falhou")
-        break
+wordle()
